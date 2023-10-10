29823414144b454de83f7da28a39b22fe8b84cc9
Abdarhman <abdarhmanalsyaed@yahoo.com>
2023-10-10 09:03:38 +0300

    Setup and page routs

---
+++ src/app/dashboard/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './page.module.css'
+
+
+const Dashboard = () => {
+  return (
+<div classNam={styles.container}>Dashboard</div>
+  )
+}
+
+export default Dashboard
